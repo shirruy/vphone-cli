4bc6114455a1980dc53faa628f032d5681becd78
shirruy <63145023+shirruy@users.noreply.github.com>
2026-09-25 05:23:19 +0800

    feat(bundle): add multi-member GNU tar primitive

--- windows/include/vphone/archive_portable.h
+++ windows/include/vphone/archive_portable.h
@@ -6,6 +6,21 @@
 extern "C" {
 #endif
 
+typedef struct vphone_archive_member {
+    const char* path;
+    const void* data;
+    size_t size;
+    const char* hardlink_target;
+} vphone_archive_member;
+
+int vphone_archive_create_gnutar(
+    const char* archive_path,
+    const vphone_archive_member* members,
+    size_t member_count,
+    char* error,
+    size_t error_size
+);
+
 int vphone_archive_create_single_gnutar(
     const char* archive_path,
     const char* stored_path,
